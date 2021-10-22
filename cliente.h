a6a323fc5f7f82aef67bc4f43e53b682ec49338f
nafernandez12 <90975885+nafernandez12@users.noreply.github.com>
2021-10-22 15:47:09 -0300

    Add files via upload

---
+++ cliente.h
@@ -0,0 +1,33 @@
+#ifndef CLIENTE_H_INCLUDED
+#define CLIENTE_H_INCLUDED
+
+struct ClienteEstructura;
+typedef struct ClienteEstructura * Cliente;
+
+char * getsNombre(Cliente a);
+char * getsApellido(Cliente a);
+int getsIdUsuario(Cliente a);
+int getsEdad(Cliente a);
+char * getsDireccion(Cliente a);
+char * getsLocalidad(Cliente a);
+char * getsProvincia(Cliente a);
+
+Cliente crearCiente();
+void setsNombre(Cliente a, char nuevoNombre[30]);
+
+void setsApellido(Cliente a, char nuevoApellido[50]);
+
+
+void setsDireccion(Cliente a, char nuevoDireccion[50]);
+
+
+void setsLocalidad(Cliente a, char nuevoLocalidad[50]);
+
+void setsProvicia(Cliente a, char nuevoProvincia[50]);
+
+
+void setsIdusuario(Cliente a, int nuevoId);
+
+void setsEdad(Cliente a, int nuevoEdad);
+
+#endif // CLIENTE_H_INCLUDED
